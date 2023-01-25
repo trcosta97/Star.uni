659d7b0306fb99cc081334cd68ac5cba6179363a
Thiago Ribeiro <trcosta97@hotmail.com>
2023-01-25 15:00:31 -0300

    controle de acesso com o token, menos no login, que gera o token

--- api/api/src/main/java/star/uni/api/infra/security/TokenService.java
+++ api/api/src/main/java/star/uni/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package star.uni.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import star.uni.api.domain.usuario.Usuario;
@@ -36,8 +37,23 @@ public class TokenService {
 
     }
 
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API uni.star")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }}
+
+
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
+
+
 
 }
